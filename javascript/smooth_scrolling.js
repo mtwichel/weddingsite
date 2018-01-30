83b95c31208079b9d4f1ce8874fd995e631246ab
Marcus Twichel <marc.twichy@gmail.com>
2018-01-30 16:45:24 -0700

    Lots of work to the ceremony jumbotron

--- javascript/smooth_scrolling.js
+++ javascript/smooth_scrolling.js
@@ -1,6 +1,30 @@
-$('.nav-link').click(function(){
+$('#ceremony-link').click(function(){
+	$('html, body').animate({
+		scrollTop: $("#ceremony").offset().top
+	}, 2000);
+	console.log("HI");
+});
+$('#our-story-link').click(function(){
 	$('html, body').animate({
 		scrollTop: $("#our-story").offset().top
 	}, 2000);
 	console.log("HI");
 });
+// $('.nav-link').click(function(){
+// 	$('html, body').animate({
+// 		scrollTop: $("#our-story").offset().top
+// 	}, 2000);
+// 	console.log("HI");
+// });
+// $('.nav-link').click(function(){
+// 	$('html, body').animate({
+// 		scrollTop: $("#our-story").offset().top
+// 	}, 2000);
+// 	console.log("HI");
+// });
+// $('.nav-link').click(function(){
+// 	$('html, body').animate({
+// 		scrollTop: $("#our-story").offset().top
+// 	}, 2000);
+// 	console.log("HI");
+// });
