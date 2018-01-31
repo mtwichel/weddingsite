9989cd340c49fd4afd853b54b8b882ed9b0c96cb
Marcus Twichel <marc.twichy@gmail.com>
2018-01-31 11:30:34 -0700

    Make hero image adaptable for different screen sizes

--- javascript/hero.js
+++ javascript/hero.js
@@ -1,13 +1,19 @@
-var jumboHeight = $('.jumbotron').outerHeight();
+var windowHeight = $(window).height();
+var windowMargin = windowHeight/5;
+var jumboHeight = windowHeight-windowMargin;
+
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+    $('#hero').css('height', jumboHeight + 'px');
+    $('#hero').css('margin-top', windowMargin + 'px');
+    $('.bg').css('height', (jumboHeight+windowMargin) + 'px');
+});
 function parallax(){
     var scrolled = $(window).scrollTop();
-    $('.bg').css('height', (jumboHeight-scrolled+200) + 'px');
+    $('.bg').css('height', (jumboHeight-scrolled+windowMargin) + 'px');
 }
 
 $(window).scroll(function(e){
     parallax();
-});
-
-$('.navbar').affix({
-  offset: {top: 100}
 });
